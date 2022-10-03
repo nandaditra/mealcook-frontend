5389001d3d4f61902e188d47760b59ffc8839120
Nanda Aditya Putra <nandaditra56@gmail.com>
2022-10-03 21:07:01 +0700

    add the grid system and background

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,23 +4,30 @@ export default function Footer() {
     return (
         <div>
           <footer>
-             <p>mealcook is a website which available some recipes of cookie from famous chefs work in best restaurant's in the world. In here, you can cook like restauran menu 5 start</p>
- 
-             <ul>
-               <li><a href={"#"}>Home</a></li>
-               <li><a href="#">About</a></li>
-               <li><a href="#">Terms</a></li>
-               <li><a href="#">Privacy Policy</a></li>
-             </ul>
+               <div className="row mx-1">
+                  <div className="col-4">
+                      <p>mealcook is a website which available some recipes of cookie from famous chefs work in best restaurant's in the world. In here, you can cook like restauran menu 5 start</p>
+                  </div>
 
-             <ul>
-               <a href=""><FontAwesomeIcon icon="fa-brands fa-facebook" />fb</a>
-               <a href=""><FontAwesomeIcon icon="fa-brands fa-instagram" />ig</a>
-               <a href=""><FontAwesomeIcon icon="fa-brands fa-twitter" />twtter</a>
-               <a href=""><FontAwesomeIcon icon="fa-brands fa-tiktok" />tiktok</a>
-             </ul>
-
-             <p>@copyright-2022 powered by hackntra technologies Ltd</p>
+                  <div className="col-4">
+                      <ul>
+                         <li><a href="#">Home</a></li>
+                         <li><a href="#">About</a></li>
+                         <li><a href="#">Terms</a></li>
+                         <li><a href="#">Privacy Policy</a></li>
+                      </ul>
+                  </div>                  
+                  
+                  <div className="col-4">
+                      <ul>
+                          <a href=""><FontAwesomeIcon icon="fa-brands fa-facebook" />fb</a>
+                          <a href=""><FontAwesomeIcon icon="fa-brands fa-instagram" />ig</a>
+                          <a href=""><FontAwesomeIcon icon="fa-brands fa-twitter" />twtter</a>
+                          <a href=""><FontAwesomeIcon icon="fa-brands fa-tiktok" />tiktok</a>
+                      </ul>
+                  </div>
+                </div>      
+              <p>@copyright-2022 powered by hackntra technologies Ltd</p>
           </footer>
         </div>
     )
