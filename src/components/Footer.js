dcba7839994c024dc7c18a323c0060bcbd412f01
Nanda Aditya Putra <nandaditra56@gmail.com>
2022-10-04 21:24:46 +0700

    editing some feauters in many part

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,28 +4,29 @@ export default function Footer() {
     return (
         <div>
           <footer>
-               <div className="row mx-1">
-                  <div className="col-4">
-                      <p>mealcook is a website which available some recipes of cookie from famous chefs work in best restaurant's in the world. In here, you can cook like restauran menu 5 start</p>
+               <div className="row mx-3 my-2">
+                  <div className="col-4 p-1">
+                      <p className="text-justify">mealcook is a website which available some recipes of cookie from famous chefs work in best restaurant's in the world. In here, you can cook like restauran menu 5 start and free realtime online</p>
                   </div>
 
                   <div className="col-4">
-                      <ul>
+                      <ul className="text-center mx-5">
                          <li><a href="#">Home</a></li>
                          <li><a href="#">About</a></li>
                          <li><a href="#">Terms</a></li>
                          <li><a href="#">Privacy Policy</a></li>
                       </ul>
-                  </div>                  
-                  
+                  </div>   
+
                   <div className="col-4">
-                      <ul>
-                          <a href=""><FontAwesomeIcon icon="fa-brands fa-facebook" />fb</a>
-                          <a href=""><FontAwesomeIcon icon="fa-brands fa-instagram" />ig</a>
-                          <a href=""><FontAwesomeIcon icon="fa-brands fa-twitter" />twtter</a>
-                          <a href=""><FontAwesomeIcon icon="fa-brands fa-tiktok" />tiktok</a>
+                       <ul className="">
+                          <a className="icon-media" href=""><FontAwesomeIcon icon="fa-brands fa-facebook" size="2x"/></a>
+                          <a className="icon-media" href=""><FontAwesomeIcon icon="fa-brands fa-instagram" size="2x"/></a>
+                          <a className="icon-media" href=""><FontAwesomeIcon icon="fa-brands fa-twitter" size="2x"/></a>
+                          <a className="icon-media" href=""><FontAwesomeIcon icon="fa-brands fa-tiktok" size="2x"/></a>
                       </ul>
-                  </div>
+                  </div>               
+                  
                 </div>      
               <p>@copyright-2022 powered by hackntra technologies Ltd</p>
           </footer>
